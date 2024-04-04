100c94bed784e6f51ec82a2b95035494c0300ff1
rikatsetya <rikatsetyagus@gmail.com>
2024-04-04 19:22:52 +0700

    "Tambahan soal 1.3"

--- Pertemuan 8/form_upload.php
+++ Pertemuan 8/form_upload.php
@@ -2,10 +2,25 @@
 <html>
     <head>
         <title>File Upload</title>
+        
     </head>
     <body>
+<!-- Soal 1.3 -->
+        <script>
+            function previewImage(event) {
+                var file = event.target.files[0];
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    var previewImage = document.getElementById("preview");
+                    previewImage.style.display = "block";
+                    previewImage.src = e.target.result;
+                }
+                reader.readAsDataURL(file);
+            }
+        </script>
         <form action="upload.php" method="post" enctype="multipart/form-data">
             <input type="file" name="fileToUpload" id="fileToUpload">
+            <img id="preview" src="#" alt="Preview" style="max-width: 200px; max-height: 200px; display: none;">
             <input type="submit" value="Upload File" name="submit">
         </form>
     </body>
--- Pertemuan 8/upload.php
+++ Pertemuan 8/upload.php
@@ -1,14 +1,14 @@
 <?php
 if (isset($_POST["submit"])){
-    $targetDirectory = "documents/"; // Direktori tujuan untuk menyimpan file 
+    $targetDirectory = "documents/"; // Direktori tujuan untuk menyimpan file
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
     $documentFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
     
-    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $allowedExtensions = array("jpeg","jpg","png","gif");
     
     $maxFileSize = 10 * 1024 * 1024;
     
-    if (in_array($documentFileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) { 
+    if (in_array($documentFileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) { 
             echo "Dokumen berhasil diunggah.";
         } else {
@@ -17,4 +17,5 @@ if (isset($_POST["submit"])){
     } else {
         echo "jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
+    
 }
